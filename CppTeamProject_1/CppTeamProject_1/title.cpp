d38bda1dfacfcafe6c6d7a70a5f733ed33a31c0a
hanbyul0103 <all007b@naver.com>
2024-06-21 00:12:45 +0900

    fix: story

--- CppTeamProject_1/CppTeamProject_1/title.cpp
+++ CppTeamProject_1/CppTeamProject_1/title.cpp
@@ -2,12 +2,22 @@
 #include <fstream>
 #include <string>
 #include <Windows.h>
+#include <conio.h>
 
 using std::cin;
 using std::cout;
 
 void MoveWindow(int x, int y);
 void ShakeWindow(int count);
+
+enum class Key {
+	SHAKE	= 49, // 1
+	CLS		= 50, // 2
+	REST	= 51, // 3
+	NARR	= 52, // 4
+	DANGER	= 53, // 5
+	WARNING	= 54, // 6
+};
 
 int main() {
 	SetConsoleOutputCP(65001);
@@ -20,23 +30,36 @@ int main() {
 	std::string line;
 
 	if (story.is_open()) {
+		int sleepTime = 0;
 		while (getline(story, line))
 		{
 			for (int i = 0; i < line.size(); i++)
 			{
-				if (line[i] == '1') {
-					ShakeWindow(20);
+				if (line[i] == (char)Key::SHAKE) {
+					ShakeWindow(10);
 				}
-				if (line[i] == '2') {
+				else if (line[i] == (char)Key::CLS) {
 					system("cls");
-					Sleep(100);
+					Sleep(1000);
+				}
+				else if (line[i] == (char)Key::REST) {
+					sleepTime = 120;
+				}
+				else if (line[i] == (char)Key::NARR) {
+					sleepTime = 30;
+				}
+				else if (line[i] == (char)Key::DANGER) {
+					sleepTime = 15;
+				}
+				else if (line[i] == (char)Key::WARNING) {
+					sleepTime = 8;
 				}
 				else cout << line[i];
-				Sleep(20);
+				Sleep(sleepTime);
 			}
 
+			Sleep(600);
 			cout << "\n";
-			Sleep(300);
 		}
 
 		story.close();
@@ -50,6 +73,12 @@ void MoveWindow(int x, int y) {
 
 void ShakeWindow(int count) {
 	int shakeCount = count;
+
+	for (int i = 0; i < shakeCount; i++)
+	{
+		MoveWindow(384 + pow(-1, i) * 2, 216 + pow(-1, i) * 2);
+		Sleep(50);
+	}
 
 	for (int i = 0; i < shakeCount; i++)
 	{
